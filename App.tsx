8da2eb78d9411fbba2f8429394681ca95349a100
kipngeno kevin <kipngenokevin254@gmail.com>
2022-12-10 12:40:06 +0300

    App.tsx Amplify update

--- App.tsx
+++ App.tsx
@@ -4,10 +4,15 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+
+Amplify.configure(awsconfig)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  
 
   if (!isLoadingComplete) {
     return null;
